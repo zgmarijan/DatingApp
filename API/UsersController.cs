840a42cdc8132dd99e5dd1cfe1c858c2507d36cc
Marijan Novak <zgmarijan@gmail.com>
2024-11-25 22:25:04 +0100

    Initial commit

---
+++ API/UsersController.cs
@@ -0,0 +1,29 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if(user == null) return NotFound();
+
+        return Ok(user);
+    }
+}
